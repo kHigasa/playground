2b8ef80f0ccb314f7c0800a604fbd84737de66cd
kHigasa <kojihigasa@gmail.com>
2018-12-04 16:31:03 +0900

    Wip

---
+++ HighSpC/chap2/variable.c
@@ -0,0 +1,12 @@
+//
+// Created by andre on 2018-12-04.
+//
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int number;
+    number = 12345;
+    printf("Number is %d\n", number);
+    return 0;
+}
