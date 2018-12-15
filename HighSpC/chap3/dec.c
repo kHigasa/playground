f9bce4f79c21b20abf9c7b418ade4eda9ebcc8a3
kHigasa <kojihigasa@gmail.com>
2018-12-15 16:29:11 +0900

    decrement

---
+++ HighSpC/chap3/dec.c
@@ -0,0 +1,29 @@
+//
+// Created by andre on 2018-12-15.
+//
+
+#include <stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+    int a, b;
+    int answer;
+
+    a = 1;
+    b = 1;
+    answer = a - -b;
+    printf("answer is %d\n", answer);
+
+    a = 1;
+    b = 1;
+    answer = a --- b;
+    printf("answer is %d\n", answer);
+
+    a = 1;
+    b = 1;
+    answer = a - -- b;
+    printf("answer is %d\n", answer);
+
+    return 0;
+}
