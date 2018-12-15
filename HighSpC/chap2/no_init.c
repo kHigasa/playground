8c181e856419f9c9c9fd9b64ce8fbf21db96adc8
kHigasa <kojihigasa@gmail.com>
2018-12-15 13:32:31 +0900

    no_init

--- HighSpC/chap2/no_init.c
+++ HighSpC/chap2/no_init.c
@@ -2,3 +2,15 @@
 // Created by andre on 2018-12-15.
 //
 
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char character;
+    int number;
+
+    printf("character is = %c\n", character);
+    printf("number is = %d\n", number);
+
+    return 0;
+}
