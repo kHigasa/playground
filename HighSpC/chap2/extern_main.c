a0143e25ac8a0981f5bb3689993fbe023c6c8de3
kHigasa <kojihigasa@gmail.com>
2018-12-15 15:06:42 +0900

    extern

---
+++ HighSpC/chap2/extern_main.c
@@ -0,0 +1,14 @@
+//
+// Created by andre on 2018-12-15.
+//
+
+#include <stdio.h>
+
+extern int global_number;
+
+int main(int argc, char *argv[])
+{
+    printf("global_number = %d\n", global_number);
+
+    return 0;
+}
---
+++ HighSpC/chap2/extern_sub.c
@@ -0,0 +1,5 @@
+//
+// Created by andre on 2018-12-15.
+//
+
+int global_number = 99;
