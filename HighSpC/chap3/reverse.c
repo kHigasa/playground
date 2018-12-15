bc574c47ef0e98206cb50f17be727f3821d8d535
kHigasa <kojihigasa@gmail.com>
2018-12-15 16:35:25 +0900

    reverse

---
+++ HighSpC/chap3/reverse.c
@@ -0,0 +1,16 @@
+//
+// Created by andre on 2018-12-15.
+//
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int n;
+
+    n = 1431655765;
+    printf("n is %d\n", n);
+    printf("~n is %d\n", ~n);
+
+    return 0;
+}
