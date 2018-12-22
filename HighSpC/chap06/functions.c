a0999f96aa29cb7e484402a50822c7d67e667c29
kHigasa <kojihigasa@gmail.com>
2018-12-22 18:35:58 +0900

    include

---
+++ HighSpC/chap06/functions.c
@@ -0,0 +1,39 @@
+//
+// Created by andre on 2018-12-22.
+//
+
+int sum(int a, int b)
+{
+    int return_value;
+
+    return_value = a + b;
+
+    return return_value;
+}
+
+int sub(int a, int b)
+{
+    int return_value;
+
+    return_value = a - b;
+
+    return return_value;
+}
+
+int mul(int a, int b)
+{
+    int return_value;
+
+    return_value = a * b;
+
+    return return_value;
+}
+
+int div(int a, int b)
+{
+    int return_value;
+
+    return_value = a / b;
+
+    return return_value;
+}
---
+++ HighSpC/chap06/functions.h
@@ -0,0 +1,11 @@
+//
+// Created by andre on 2018-12-22.
+//
+
+int sum(int, int);
+
+int sub(int, int);
+
+int mul(int, int);
+
+int div(int, int);
