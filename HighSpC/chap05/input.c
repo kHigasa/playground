2d0900fb6a33928c7b57cfd00468f77359a5ae8f
kHigasa <kojihigasa@gmail.com>
2018-12-22 16:50:51 +0900

    input

---
+++ HighSpC/chap05/input.c
@@ -0,0 +1,18 @@
+//
+// Created by andre on 2018-12-22.
+//
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char one_string[16];
+
+    printf("input> ");
+
+    fgets(one_string, 16, stdin);
+
+    printf("Your input is %s", one_string);
+
+    return 0;
+}
