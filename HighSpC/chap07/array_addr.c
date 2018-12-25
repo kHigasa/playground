8c5e67251371a0d6b1d50fd31a723be915ce800e
kHigasa <kojihigasa@gmail.com>
2018-12-25 18:19:32 +0900

    array address

---
+++ HighSpC/chap07/array_addr.c
@@ -0,0 +1,18 @@
+//
+// Created by andre on 2018-12-25.
+//
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    double double_array[2] = {0.1, 0.2};
+    double *ptr;
+
+    ptr = double_array;
+    printf("*(ptr + 0) is %f\n", *(ptr + 0)); // printf("ptr[0] is %f\n", ptr[0]);
+    printf("*(ptr + 1) is %f\n", *(ptr + 1));
+    printf("double_array[0] is %f\n", double_array[0]); // printf("*(double_array + 0) is %f\n", *(double_array + 0));
+    printf("double_array[1] is %f\n", double_array[1]);
+
+    return 0;
+}
