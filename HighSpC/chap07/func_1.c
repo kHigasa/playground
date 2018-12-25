2d29b011c263650342e4c154843842f7b2e106b9
kHigasa <kojihigasa@gmail.com>
2018-12-25 18:41:36 +0900

    pass address

---
+++ HighSpC/chap07/func_1.c
@@ -0,0 +1,32 @@
+//
+// Created by andre on 2018-12-25.
+//
+
+#include <stdio.h>
+
+int sum(int *, int *, int *);
+
+int sum(int *a, int *b, int *ans) {
+    *ans = *a + *b;
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    int num_1;
+    int num_2;
+    int *answer;
+    int ans;
+
+    num_1 = 1;
+    num_2 = 2;
+    answer = &ans;
+
+    if (sum(&num_1, &num_2, answer) != 0) {
+        printf("error\n");
+    }
+
+    printf("answer = %d\n", *answer);
+
+    return 0;
+}
