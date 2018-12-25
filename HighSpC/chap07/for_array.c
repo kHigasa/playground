cd986f6c8253da9d5f920778b6d504034c0fbe2e
kHigasa <kojihigasa@gmail.com>
2018-12-25 16:10:38 +0900

    for array

---
+++ HighSpC/chap07/for_array.c
@@ -0,0 +1,18 @@
+//
+// Created by andre on 2018-12-25.
+//
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int number_array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    int i, answer;
+
+    for (i = 0, answer = 0; i < (sizeof(number_array) / sizeof(number_array[0])); i++) {
+        answer += number_array[i];
+    }
+    printf("answer = %d\n", answer);
+
+    return 0;
+}
