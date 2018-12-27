809e6f3c2842fd08f157d61a172d94c4e91a4a7b
higasa <higasa@pepabo.com>
2018-12-27 18:31:52 +0900

    eval

---
+++ Camelus/chap02/eval.ml
@@ -0,0 +1,15 @@
+max_int;;
+min_int;;
+max_float;;
+min_float;;
+107 mod 3;;
+3 + int_of_float 3.14;;
+float_of_int 3 +. 3.14;;
+infinity;;
+neg_infinity;;
+nan;;
+infinity /. neg_infinity;;
+'a';; (* char *)
+"a";; (* string *)
+"Hello," ^ " world!";; (* 文字列結合の infix operator *)
+("Hello," ^ " world!").[10];;
