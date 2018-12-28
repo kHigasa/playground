ca506b1110aef452fb3537c2a60630cab05a1174
higasa <higasa@pepabo.com>
2018-12-28 17:16:15 +0900

    newton method

--- Camelus/chap03/func.ml
+++ Camelus/chap03/func.ml
@@ -81,3 +81,18 @@ let (^-^) x y = x * 2 + y * 3;; (* def infix operator *)
 
 let ( !! ) x = x + 1;; (* prefix operator *)
 !!4;;
+
+(* Newton-Raphson method: i miss kumo's little brother😭 *)
+let deriv f =
+  let dx = 0.1e-10 in fun x -> (f(x +. dx) -. f(x)) /. dx;;
+deriv (fun x -> x *. x *. x) 3.0;;
+let fixpoint f init = (* fixed-point; f: A -> A *)
+  let threshold = 0.1e-10 in
+  let rec loop x =
+    let next = f x in
+    if abs_float (x -. next) < threshold then x
+    else loop next in loop init;;
+let newton_transform f = fun x -> x -. f(x) /. (deriv f x);;
+let newton_method f guess = fixpoint (newton_transform f) guess;;
+let square_root a = newton_method (fun x -> x *. x -. a) 1.0;;
+square_root 5.0;;
