d9e59517a23851305f66a397c0e4ff0e1b59c3ee
higasa <higasa@pepabo.com>
2018-12-28 18:52:34 +0900

    combinator

--- Camelus/chap04/infer.ml
+++ Camelus/chap04/infer.ml
@@ -11,3 +11,17 @@ twice (fun s -> "<" ^ s ^ ">") "abc";;
 twice twice (fun x -> x + 1) 3;;
 twice twice (fun s -> "<" ^ s ^ ">") "abc";;
 let fourtimes x = twice twice x in (fourtimes (fun s -> "<" ^ s ^ ">") "abc");;
+(* λ-calculus, combinatorylogic *)
+(* Combinator 明示的なパラメータの導入なく単純な関数を組み合わせて複雑な関数を構成できる *)
+let ($) f g x = f (g x);;
+let f = ( ~-. ) $ sqrt in f 2.0;;
+(* I-combinator:恒等関数id *)
+(sqrt $ id) 3.0;;
+(id $ sqrt) 3.0;;
+(* K-combinator:定数関数を構成し、K xは必ずxを返す *)
+let k x y = x;;
+let const17 = k 17 in const17 4.0;;
+(* S-combinator:関数合成の・を一般化 *)
+let s x y z = x z (y z);;
+(* I-combinator == S K K *)
+s k k 5;;
