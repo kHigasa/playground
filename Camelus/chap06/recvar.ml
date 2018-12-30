ebfe82ce06feeae4f99952a48516c23810f4fd1b
kHigasa <kojihigasa@gmail.com>
2018-12-31 00:17:44 +0900

    variant

--- Camelus/chap06/recvar.ml
+++ Camelus/chap06/recvar.ml
@@ -41,3 +41,38 @@ let similar x y =
     | (Rectangle (11, 12), Rectangle (13, 14)) -> (13 * 12 - 14 * 11) = 0
   | _ -> false;;
 similar (Rectangle (2, 4)) (Rectangle (1, 2));;
+(* 列挙型 *)
+type color = Black | Blue | Red | Magenta | Green | Cyan | Yellow | White;;
+let reverse_color = function
+  Black -> White | Blue -> Yellow | Red -> Cyan | Magenta -> Green | Green -> Magenta | Cyan -> Red | Yellow -> Blue | White -> Black;;
+(* 再帰ヴァリアント型 *)
+(* 自然数を表す型nat *)
+type nat = Zero | OneMoreThan of nat;;
+let zero = Zero and two = OneMoreThan (OneMoreThan Zero);;
+(* 足し算 *)
+let rec add m n =
+  match m with Zero -> Zero | OneMoreThan m' -> OneMoreThan (add m' n);;
+(* 整数リスト *)
+type intlist = INil | ICons of int * intlist;;
+(* 奇数・偶数の相互再帰的定義 *)
+type even = Zero | OMT_E of odd and odd = OMT_O of even;;
+let rec o_plus_e (OMT_O e1) e2 = OMT_O (e_plus_e e1 e2) and e_plus_e e1 e2 =
+  match e1 with Zero -> e2 | OMT_E o -> OMT_E (o_plus_e o e2);;
+(* 多相的ヴァリアント型 *)
+type 'a mylist = Nil | Cons of 'a * 'a mylist;;
+(* 多相的レコード型 *)
+type 'a with_location = {loc_x: float; loc_y: float; body: a'};;
+{loc_x = 0.0; loc_y = 0.0; body = Point};;
+type ('a, 'b) list_with_tail = Nil of b' | Cons of 'a * ('a, 'b) list_with_tail;;
+Cons (2, Cons(3, Nil("end")));;
+(* オプション型 *)
+type 'a option = None | Some of 'a
+let fact n =
+  let rec fact' n =
+    if n = 0 then 1
+    else n * fact' (n-1) in
+    if n < 0 then None else Some (fact' n);;
+fact 3;;
+fact (-10);;
+type ('a, 'b) sum = Left of 'a | Right of 'b;;
+[Left 3; Right "foo"];;
