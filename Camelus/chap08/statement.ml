86e1a123e25fd739edbe78c68414f771981952d5
kHigasa <kojihigasa@gmail.com>
2019-01-05 11:55:45 +0900

    queue

--- Camelus/chap08/statement.ml
+++ Camelus/chap08/statement.ml
@@ -119,3 +119,29 @@ let rec iter f = function
     [] -> ()
   | a :: rest -> begin f a; iter f rest end;;
 iter (fun s -> print_string "Station:"; print_endline s) ["Tokyo"; "Shinagawa"; "Shin-Yokohama"; "Nagoya"; "Kyoto"; "Shin-Osaka"];;
+(* Queue *)
+type 'a mlist = MNil | MCons of 'a * 'a mlist ref;;
+type 'a queue = {mutable head : 'a mlist; mutable tail : 'a mlist};;
+let create () = {head = MNil; tail = MNil};;
+let q : int queue = create ();;
+let add a = function
+    {head = MNil; tail = MNil} as q -> let c = MCons (a, ref MNil)
+    in q.head <- c; q.tail <- c
+  | {tail = MCons (_, next)} as q -> let c = MCons (a, ref MNil)
+    in next := c; q.tail <- c
+  | _ -> failwith "enqueue: input queue broken";;
+add 1 q; add 2 q; add 3 q;;
+q;;
+let peek = function
+    {head = MNil; tail = MNil} -> failwith "hd: queue is empty"
+  | {head = MCons (a, _)} -> a
+  | _ -> failwith "hd: queue is broken";;
+peek q;;
+let take = function
+    {head = MNil; tail = MNil} -> failwith "dequeue: queue is empty"
+  | {head = MCons (a, next)} as q -> q.head <- !next; a
+  | _ -> failwith "dequeue: queue is broken";;
+take q;;
+take q;;
+add 4 q; take q;;
+ignore (take q); add 5 q; peek q;;
