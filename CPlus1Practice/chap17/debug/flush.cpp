5c7daa0135ad88fed9670ff595b3f0f2fedc188a
kHigasa <kojihigasa@gmail.com>
2019-01-10 22:03:01 +0900

    debug

---
+++ CPlus1Practice/chap17/debug/flush.cpp
@@ -0,0 +1,17 @@
+//
+// Created by andre on 2019-01-10.
+//
+
+#include <iostream>
+int main()
+{
+    int i,j;	/* two random integers */
+
+    i = 1;
+    j = 0;
+    std::cout << "Starting\n";
+    std::cout << "Before divide...";
+    i = i / j;	// divide by zero error
+    std::cout << "After\n";
+    return(0);
+}
