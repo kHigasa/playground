792e1a4d1ee94f7b4b31859e1ec31776877842ad
kHigasa <kojihigasa@gmail.com>
2019-01-10 21:23:29 +0900

    io

---
+++ CPlus1Practice/chap16/io/io.cpp
@@ -0,0 +1,28 @@
+//
+// Created by andre on 2019-01-10.
+//
+
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    int number = 12;	// A number to output
+    float real = 12.34;	// A real number
+
+    std::cout << "123456789012345678901234567890\n"; // output ruler
+    std::cout << number << "<-\n";
+    std::cout << std::setw(5) << number << "<-\n";
+    std::cout << std::setw(5) << std::setfill('*') <<
+              number << "<-\n";
+    std::cout << std::setiosflags(std::ios::showpos|std::ios::left) <<
+              std::setw(5) << number << "<-\n";
+
+    std::cout << real << "<-\n";
+    std::cout << std::setprecision(1) <<
+              std::setiosflags(std::ios::fixed) << real << "<-\n";
+
+    std::cout << std::setiosflags(std::ios::scientific) << real << "<-\n";
+    return (0);
+}
+
---
+++ CPlus1Practice/chap16/log/log.cpp
@@ -0,0 +1,15 @@
+//
+// Created by andre on 2019-01-10.
+//
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+void log_message(const std::string& msg)
+{
+    std::ofstream out_file("data.log", std::ios::out|std::ios::app|std::ios::ate);
+    if (out_file.fail())
+        return;
+    out_file << msg << std::endl;
+}
