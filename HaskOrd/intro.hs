54b2dc308205d4af57d97878643e82d0868aff26
khigasa <kojihigasa@gmail.com>
2019-03-26 17:05:45 +0900

    Tue Mar 26 17:05:45 JST 2019

--- HaskOrd/intro.hs
+++ HaskOrd/intro.hs
@@ -1,3 +1,6 @@
+import Data.List (nub)
+import qualified Data.Map as Map
+
 --factorial :: Integer -> Integer
 --factorial n = product [1..n]
 
@@ -15,4 +18,30 @@ factorial n = case n of 0 -> 1
 
 calcBmis :: [(Double, Double)] -> [Double]
 calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
+
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+    | even n = n : chain (n `div` 2)
+    | odd n  = n : chain (n * 3 + 1)
+
+lochain :: Int
+lochain = length (filter (\xs -> length xs > 15)
+                         (map chain [1..100]))
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = foldr (\x acc -> f x : acc) [] xs
+
+lounqnum :: (Eq a) => [a] -> Int
+lounqnum = length . nub
+
+findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey key xs = foldr
+                   (\(k, v) acc -> if key == k then Just v else acc)
+                   Nothing xs
 
