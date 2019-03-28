b53241747daad96a3335e83ba9f0367e0453b66a
khigasa <kojihigasa@gmail.com>
2019-03-28 19:05:25 +0900

    Thu Mar 28 19:05:24 JST 2019

---
+++ HaskOrd/todo.hs
@@ -0,0 +1,46 @@
+import System.Environment
+import System.IO
+import System.Directory
+import Data.List
+import Control.Exception
+
+dispatch :: String -> [String] -> IO ()
+dispatch "add" = add
+dispatch "view" = view
+dispatch "remove" = remove
+
+main = do
+    (command:argList) <- getArgs
+    dispatch command argList
+
+add :: [String] -> IO ()
+add [fileName, todoTask] = appendFile fileName (todoTask ++ "\n")
+
+view :: [String] -> IO ()
+view [fileName] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line)
+                                    [0..] todoTasks
+    putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line)
+                                    [0..] todoTasks
+    putStrLn "There are your todo tasks:"
+    mapM_ putStrLn numberedTasks
+    let number = read numberString
+        newTodoTasks = unlines $ delete (todoTasks !! number) todoTasks
+    bracketOnError (openTempFile "." "temp")
+        (\(tempName, tempHandle) -> do
+            hClose tempHandle
+            removeFile tempName)
+        (\(tempName, tempHandle) -> do
+            hPutStr tempHandle newTodoTasks
+            hClose tempHandle
+            removeFile fileName
+            renameFile tempName fileName)
+
